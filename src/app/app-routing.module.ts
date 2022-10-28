22010bd063c1418444c8df903418e7c8693de689
vanessahenao124 <vanessahenao124@gmail.com>
2022-10-28 08:49:24 -0500

    Se ve que es Angular y que contiene

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+//Los @ son anotaciones o decoradores de clase, le permiten inyectar funcionalidades al objeto que tienen abajo, elementos
+// y funcionalidades adicionales 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
